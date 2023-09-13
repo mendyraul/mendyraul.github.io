ff4b7adaaffd632a8adfd17e34743d6a8e3942d4
mendyraul <100150860+mendyraul@users.noreply.github.com>
2023-09-13 15:38:45 -0400

    index.php

--- index.php
+++ index.php
@@ -13,12 +13,13 @@ Licence URI: http://www.os-templates.com/template-terms
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <!-- Topicon-->
-        <link rel="icon" type="image/x-icon" href="/mendyraul.github.io/assets/img/IMG7_914.JPG"/>
+        <link rel="icon" type="image/x-icon" href="assets/img/IMG7_914.JPG"/>
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link rel="stylesheet" href="/mendyraul.github.io//styles/layout.css" type="text/css" />
+        <link rel="stylesheet" href="styles/layout.css" type="text/css" />
+        <link rel="stylesheet" href="styles/styles2.css" type="text/css" />
         <!-- Bootstrap core JS-->
-        <script type="text/javascript" src="/mendyraul.github.io/scripts/jquery.min.js"></script>
-        <script type="text/javascript" src="/mendyraul.github.io/scripts/jquery.slidepanel.setup.js"></script>
+        <script type="text/javascript" src="scripts/jquery.min.js"></script>
+        <script type="text/javascript" src="scripts/jquery.slidepanel.setup.js"></script>
     </head>
     <body id="top">
         <div class="wrapper col1">
@@ -39,57 +40,69 @@ Licence URI: http://www.os-templates.com/template-terms
         <div class="wrapper col2">
             <div id="header">
                 <p class="toggler"><a id="slideit" href="#socialise">Click Here to Socialize!</a><a id="closeit" style="display:none;" href="#socialise">Close Panel</a></p>
-                <div id="logo">
-                    <p>Raul Mendy</p>
-                </div>
-                <div id="topnav">
-                <ul>
-                <ul>
-                    <li><a href="/mendyraul.github.io/index.php">Homepage</a></li>
-                    <li><a href="/mendyraul.github.io/pages/projects.html">Projects </a></li>
-                    <li><a href="/mendyraul.github.io/pages/blog.html">Blog </a></li>
-                    <li class="active" ><a href="/mendyraul.github.io/pages/about-me.html">About Me</a></li>
-                    <li><a href="pages/style-demo.html">Style Demo</a></li>
-                    <li class="last"><a href="pages/full-width.html">Full Width</a></li>
-                </ul>
-                </div>
+
+                <element id="topnav">
+                    <ul>
+                        <li class="active"><a href="index.php">Homepage</a></li>
+                        <li><a href="/mendyraul.github.io/pages/projects.html">Projects </a></li>
+                        <li><a href="/mendyraul.github.io/pages/blog.html">Blog</a></li>
+                        <li><a href="/mendyraul.github.io/pages/about-me.html">About Me</a></li>
+                        <li><a href="pages/style-demo.html">Style Demo</a></li>
+                        <li class="last" ><a href="pages/full-width.html">Full Width</a></li>
+
+                    </ul>
+                </element>
                 <br class="clear" />
             </div>
         </div>
         <!-- ####################################################################################################### -->
-        <div class="wrapper col4" id="About-Me">
+        <div class="wrapper col3">
+            <div class="container">
+                <img src="assests/img/welcome-3345143_1280.jpg" style="width:100%;">
+                <div class="text-block">
+                    <h4> Explore More </h4>
+                </div>
+            </div>
+        </div>
+        <!-- ####################################################################################################### -->
+        <div class="wrapper col4" id="">
             <div class="overview">
-                <h2 id="title">More information</h2>
-                <style>
-                    img {
-                        max-width: 100%;
-                        height: auto;
-                        align-content: center;
-                    }
-                </style>
+                <h2 id="title">Welcome</h2>
+
                 <ul>
-                    <div>
-                        <h2>A Little Something About Myself </h2>
-                        <img class="imgl" src="/mendyraul.github.io//assests/img/IMG_Webpage.jpeg" alt="" />
-                        <!-- <style> .img {height: 80px; width: 80px;} </style> -->
-                        <p>My name is Raul Mendy and I am a student at Florida Atlantic University. I am majoring Computer Science and minoring in Cybersecruity. If you have any inquiries please do not hesitate to reach out!</p>
-                    </div>
+
                 </ul>
                 <br class="clear" />
             </div>
+        </div>
+        <!-- ####################################################################################################### -->
+        <div class="wrapper col3">
+            <p>Section ilwdhBJFCQIWDHBC
+                QIWEJK
+                BCIQWEDBJCQWLKDSJBCLQK
+                WADJ.SBC
+                QKW.DSJBCKQLWDSBC
+                LKQWADSBCKLQWREAHBDCLR
+                JQEBCVHQJR
+                BDCQJERWHD
+
+                BCLEQW</p>
         </div>
         <!-- ####################################################################################################### -->
         <div class="wrapper col5">
             <div id="footer">
-                <div class="box contactdetails">
-                <h2>My Contact Details </h2>
-                <ul>
-                    <li>Fort Lauderdale, FL</li>
-                    <li>Tel: (954)-294-4244</li>
-                    <li><a href="mailto:rmendy2019@fau.edu"> Email:</a> rmendy2019@fau.edu</li>
-                    <li class="last"><a href="/mendyraul.github.io/assests/Raul Mendy Resume.pdf">Resume</a></li>
-                </ul>
+                <div class="box1">
+
                 </div>
+                <div class="box contactdetails">
+                    <h2>My Contact Details </h2>
+                    <ul>
+                        <li>Fort Lauderdale, FL</li>
+                        <li>Tel: (954)-294-4244</li>
+                        <li><a href="mailto:rmendy2019@fau.edu"> Email:</a> rmendy2019@fau.edu</li>
+                        <li class="last"><a href="assests/Raul Mendy Resume.pdf">Resume</a></li>
+                    </ul>
+                    </div>
                 </div>
                 <br class="clear" />
             </div>
@@ -97,7 +110,7 @@ Licence URI: http://www.os-templates.com/template-terms
         <!-- ####################################################################################################### -->
         <div class="wrapper col6">
         <div id="copyright">
-            <p class="fl_left">Copyright &copy; 2014 - All Rights Reserved - <a href="#">Domain Name</a></p>
+            <p class="fl_left">Copyright &copy; 2014 - All Rights Reserved - os-templates</p>
             <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
             <br class="clear" />
         </div>
